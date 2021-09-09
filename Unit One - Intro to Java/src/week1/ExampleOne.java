3eb65f17484d02ada547df6a59332d6f7baecffb
Yola_Xu <zxu@bayviewglen.ca>
2021-09-09 15:36:04 -0400

    Initial Commit

---
+++ Unit One - Intro to Java/src/week1/ExampleOne.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Welcome to ICS3U!!!");
+        System.out.println("Welcome to ICS3U!!!");
+        System.out.println("Welcome to ICS3U!!!");
+
+    }
+}
