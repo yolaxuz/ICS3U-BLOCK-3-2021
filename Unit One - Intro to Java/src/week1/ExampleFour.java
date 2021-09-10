98cff5c31d40fbe81ba59ea2e7fc992f0778f4e7
Yola_Xu <zxu@bayviewglen.ca>
2021-09-10 13:22:30 -0400

    Integers class 2

---
+++ Unit One - Intro to Java/src/week1/ExampleFour.java
@@ -0,0 +1,25 @@
+package week1;
+
+/**
+ * Variables - used to store data 
+ * Primitive Data Types - (int / double / char / boolean)
+ * Assignment / Assignment Operator - used to store values/data in variables 
+ */
+
+public class ExampleFour {
+
+    public static void main(String[] args) {
+        int markOne, markTwo, markThree; // decalred 3 integer variables 
+
+        markOne = 75;    // markONe is assigned 75
+        markTwo = 80;    
+        markThree = 87; 
+
+        int average = (markOne + markTwo + markThree) / 3; 
+
+        System.out.println(average);
+        System.out.println("The average is: " + average); // string concatanation - joined the string with something else
+
+    }
+    
+}
---
+++ Unit One - Intro to Java/src/week1/ExampleThree.java
@@ -0,0 +1,18 @@
+package week1;
+
+
+
+/**
+ * Escape Sequences 
+ */
+public class ExampleThree {
+    
+    public static void main(String[] args) {
+     // System.out.println("This is "very" imporotant!"); // compile error because double quote encapsulate a string literal and we added extra quotes
+     
+    System.out.println("this is \"very\" important!");
+    
+    //System.out.println("this is \very important!"); // escape the quote by creating an escape sequence using \ \ 
+    }
+    
+}
