59a0ee78b7e1afa1dc0bce9a30849f73d5185303
Yola_Xu <zxu@bayviewglen.ca>
2021-09-16 13:02:36 -0400

    Student Class Example

--- Unit2- using objects/week2/Student.java
+++ Unit2- using objects/week2/Student.java
@@ -1,63 +1,57 @@
-package week2; 
+package week2;
 
-public class Studen {
-
-
-    /**
+/**
  * A class is a blue print for something that we can model and create variables
  * to use in our code.
  * 
  * This code (class) defines what a Student is
  */
-public class Student { 
+public class Student {
 
-    /**
-     * Attributes that define a student The state of a specific student is what is
-     * in the attributes at a specific point
-     */
-    private String name;
-    private String studentNumber;
-    private int grade;
-  
-    /**
-     * 
-     * The method with the same name as the class is used to instantiate (create) a
-     * Student Object
-     * 
-     * Constructor - same name as the class and its purpose is to instantiate a
-     * instance of the class (creates a state and places it in its initial state)
-     */
-    public Student(String name, String studentNumber, int grade) {
-      this.name = name;
-      this.studentNumber = studentNumber;
-      this.grade = grade;
-    }
-  
-    /**
-     * displayName, displayStudentNumber, increaseGrade, displayGrade The methods
-     * (actions) in a class define behaviour for the class
-     */
-    public void displayName() {
-      System.out.println(name);
-    }
+  /**
+   * Attributes that define a student The state of a specific student is what is
+   * in the attributes at a specific point
+   */
+  private String name;
+  private String studentNumber;
+  private int grade;
 
-    /** 
-     * A void methos performs a task and returns NOthin
-     * non-static means that each object gets its own version (ex. name, studentNumber, grade,)
-     */
-  
-    public void displayStudentNumber() {
-      System.out.println(studentNumber);
-    }
-  
-    public void increaseGrade() {
-      grade++;
-    }
-  
-    public void displayGrade() {
-      System.out.println(grade);
-    }
-  
+  /**
+   * 
+   * The method with the same name as the class is used to instantiate (create) a
+   * Student Object
+   * 
+   * Constructor - same name as the class and its purpose is to instantiate a
+   * instance of the class (creates a state and places it in its initial state)
+   */
+  public Student(String name, String studentNumber, int grade) {
+    this.name = name;
+    this.studentNumber = studentNumber;
+    this.grade = grade;
   }
-    
+
+  /**
+   * displayName, displayStudentNumber, increaseGrade, displayGrade The methods
+   * (actions) in a class define behaviour for the class
+   */
+  public void displayName() {
+    System.out.println(name);
+  }
+
+  /**
+   * A void method performs a task and returns NOTHING non-static means that each
+   * OBject gets its own version (ex. name, studentNumber, grade, )
+   */
+  public void displayStudentNumber() {
+    System.out.println(studentNumber);
+  }
+
+  public void increaseGrade() {
+    grade++;
+  }
+
+  public void displayGrade() {
+    System.out.println(grade);
+  }
+
 }
