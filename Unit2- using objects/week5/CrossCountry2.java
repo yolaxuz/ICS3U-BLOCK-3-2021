63b7c1be62fe9f27416396f0d71a05148d5d7cea
Yola_Xu <zxu@bayviewglen.ca>
2021-10-07 09:59:47 -0400

    cross country

--- Unit2- using objects/week5/CrossCountry2.java
+++ Unit2- using objects/week5/CrossCountry2.java
@@ -1,12 +1,12 @@
-   import java.util.Scanner;
+   import java.util.Scanner; // imports the java scanner 
 
 public class CrossCountry2 {
-   public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
-      processRunner(in);
-      processRunner(in);
-      processRunner(in);
-      in.close();
+   public static void main(String[] args) { // the main 
+      Scanner in = new Scanner(System.in); // input 
+      processRunner(in); // runs processRunner with the input 
+      processRunner(in);// runs processRunner with the input 
+      processRunner(in);// runs processRunner with the input 
+      in.close(); // ends the input 
    }
 
    /**
@@ -14,46 +14,46 @@ public class CrossCountry2 {
     * @param in
     */
    private static void processRunner(Scanner in) {
-      String firstName, lastName;
-      String mileOne, mileTwo, finishTime;
-      String splitTwo, splitThree;
+      String firstName, lastName; // setting the first namd and last name as a string 
+      String mileOne, mileTwo, finishTime; // setting the mile one mile two and finish time as a string
+      String splitTwo, splitThree; // setting splittwo and split three as a string
 
       /**
        * get the name and mleOne, mileTwo, finishTime from the user
        */
 
-      System.out.print("Enter your first name: "); 
-      firstName = in.nextLine(); 
-      System.out.print("Enter your last name: "); 
-      lastName = in.nextLine(); 
-      System.out.print("Enter your first mile: ");
-      mileOne = in.nextLine(); 
-      System.out.print("Enter your second mile: "); 
-      mileTwo = in.nextLine(); 
-      System.out.print("Enter your time finished: "); 
-      finishTime = in.nextLine(); 
+      System.out.print("Enter your first name: "); // print "Enter your first name: "ask input for firstName
+      firstName = in.nextLine(); // set the input as firstName
+      System.out.print("Enter your last name: "); // print "Enter your last name: " ask input for lastName
+      lastName = in.nextLine(); // set the input as lastName 
+      System.out.print("Enter your first mile: "); // print "Enter your first mile: " ask input for mileOne
+      mileOne = in.nextLine(); // set the input as mileOne
+      System.out.print("Enter your second mile: "); // print "Enter your second mile: " ask input for mileTwo
+      mileTwo = in.nextLine(); // set input as mileTwo 
+      System.out.print("Enter your time finished: "); // print "Enter your time finished: " ask input for finishTime
+      finishTime = in.nextLine(); // set input as finishTime 
 
-      splitTwo = subtractTimes(mileTwo, mileOne);
-      splitThree = subtractTimes(finishTime, mileTwo);
+      splitTwo = subtractTimes(mileTwo, mileOne); // set splitTwo to substractTIme 
+      splitThree = subtractTimes(finishTime, mileTwo); // set splitThree to subtractTime 
 
       /**
        * Display a summary for the runner
        */
 
-       System.out.println(firstName + lastName); 
-       System.out.println("Split One: " + mileOne); 
-       System.out.println("Split Two: " + splitTwo); 
-       System.out.println("Split Three: "+ splitThree); 
+       System.out.println(firstName + lastName); // display firstName and lastName and enter to next line
+       System.out.println("Split One: " + mileOne); // display "Split One: " and mileOne and enter to next line
+       System.out.println("Split Two: " + splitTwo); // display "Split Two: " and splitTwo and enter to next line
+       System.out.println("Split Three: "+ splitThree); // display "Split Three: " and splitTHree and enter to next line
 
    }
 
    private static String subtractTimes(String endTime, String startTime) {
-      double endInSeconds = convertToSeconds(endTime);
-      double startInSeconds = convertToSeconds(startTime);
+      double endInSeconds = convertToSeconds(endTime); // sets endInSeconds equal to convertToSeconds insert with endTime
+      double startInSeconds = convertToSeconds(startTime); // sets startInSeconds to convertTOSeconds inert with startTime 
 
-      double diffInSeconds = endInSeconds - startInSeconds;
+      double diffInSeconds = endInSeconds - startInSeconds; // set diffInSeconds as startInSeconds subtracted from endInSeconds 
 
-      return convertToTime(diffInSeconds);
+      return convertToTime(diffInSeconds); // return converToTime and insert by diffInSeconds 
    }
 
    private static String convertToTime(double diffInSeconds) {
