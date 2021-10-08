8a23918f2159ad9fbc3f0f69be25fcd38d36f947
Yola_Xu <zxu@bayviewglen.ca>
2021-10-07 21:10:17 -0400

    Crosscountry

--- Unit2- using objects/week5/CrossCountry2.java
+++ Unit2- using objects/week5/CrossCountry2.java
@@ -40,7 +40,7 @@ public class CrossCountry2 {
        * Display a summary for the runner
        */
 
-       System.out.println(firstName + lastName); // display firstName and lastName and enter to next line
+       System.out.println(firstName + " " + lastName); // display firstName and lastName and enter to next line
        System.out.println("Split One: " + mileOne); // display "Split One: " and mileOne and enter to next line
        System.out.println("Split Two: " + splitTwo); // display "Split Two: " and splitTwo and enter to next line
        System.out.println("Split Three: "+ splitThree); // display "Split Three: " and splitTHree and enter to next line
@@ -58,24 +58,25 @@ public class CrossCountry2 {
 
    private static String convertToTime(double diffInSeconds) {
         // return String.format("%d:%06.3f", minutes, seconds1);
-        return String.format("%d:%06.3f", getMinutes(diffInSeconds), getSeconds(diffInSeconds));
+        return String.format("%d:%06.3f", getMinutes(diffInSeconds), getSeconds(diffInSeconds)); 
+        // return the format of %d:%06.3f with minutes having the input of diffInseconds and seconds with theinput of diffInSeconds
       // create getMinutes and getSeconds functions to use
    }
    public static int getMinutes(double totalSecond){ 
-       int min = (int)(totalSecond/60); 
-        return min; 
+       int min = (int)(totalSecond/60); // set min as the totalSecond divided by 60
+        return min; // return min
    }
 
    public static double getSeconds(double totalSecond) {
-       double sec = (totalSecond%60); 
-       return sec; 
+       double sec = (totalSecond%60); // set sec as double and se it as totalSecond module by 60
+       return sec; // return sec 
    }
 
    private static double convertToSeconds(String time) {
-       int getColon = time.indexOf(":"); 
-       double min = Double.valueOf(time.substring(0, getColon)); 
-       double sec = Double.valueOf(time.substring(0, getColon));
-       return (min * 60) + sec;
+       int getColon = time.indexOf(":"); // set getColon as ":" 
+       double min = Double.valueOf(time.substring(0, getColon)); // Setting min as a double and giving it as time and having a parameter from 0 to colon
+       double sec = Double.valueOf(time.substring(0, getColon)); //Setting sec as a double and giving it as time  and having a parameter from 0 to colon
+       return (min * 60) + sec; // returns mim into sec plus sec 
    }
     
 }
---
+++ unit 3/src/Whileloops.java
@@ -0,0 +1,25 @@
+public class Whileloops {
+
+public static void main(String[] args) {
+    exampleOne(); 
+    exampleTwo(); 
+    int numVowels = countVowels("This is a sentence with vowels");
+    System.out.println(numVowels); 
+    
+}
+    private static int countVowels(String string) { 
+        int numVwoels = 0; 
+        String vowels = "AEIOUaeiou"; 
+        int index = 0; 
+
+        while(index < str.length()) { 
+            String nextChar = str.substring(index, index + 1); 
+        }
+
+    }
+
+    private static void exampleTwo(){
+
+    }
+}
+
